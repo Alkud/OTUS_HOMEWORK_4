c52101af1567946a72c310fb7f2f3cef69cde870
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-01 10:10:53 +0000

    Deploy Alkud/OTUS_HOMEWORK_4 to github.com/Alkud/OTUS_HOMEWORK_4.git:gh-pages

---
+++ html/search/all_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['otus_5fhw_5f4_5ftest_2ecpp',['otus_hw_4_test.cpp',['../otus__hw__4__test_8cpp.html',1,'']]]
+];
---
+++ html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['gettupletail',['getTupleTail',['../process_8h.html#a6a2cec771bd6722eb24c3d627c43b72e',1,'process.h']]],
+  ['gettupletail_5fhelper',['getTupleTail_Helper',['../process_8h.html#a9ebf3750670de2c693b96766d4bed755',1,'process.h']]]
+];
